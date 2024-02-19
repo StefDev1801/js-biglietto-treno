4b0751093d41cb3015ed9bb8b0cd90fb7ec90bb0
StefDev1801 <stefano200118@gmail.com>
2024-02-19 15:30:02 +0100

    add variables age and km  to main js

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+//- Chiedere chilometri che il passeggiero dovrà percorrere
+const kmToTravel =  prompt("Quanti chilometri deve percorrere?")
+console.log(kmToTravel);
+//- Chiedere età del passeggiero
+const ageCostumer = prompt ("Inserisca la sua età")
+console.log(ageCostumer);
+//<!-- - avere una variabile con il prezzo (€0.21/km) -->
+//- SE il passeggiero ha 17 anni o meno applicare lo sconto del 20%
+//    - OPPURE SE il passeggiero ha più o uguale a 65 anni applicare lo sconto del 40%
+//        - OPPURE far pagare prezzo pieno
+//- Far visualizzare l'output desiderato con massimo due cifre decimali e il simbolo €
