a8b43acafeef7035e69816e0aa198019d136cb00
StefDev1801 <stefano200118@gmail.com>
2024-02-19 16:17:26 +0100

    add output in forma umana and round to 2 decimal numbers

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,18 +4,21 @@ console.log(kmToTravel);
 //- Chiedere età del passeggiero
 const ageCostumer = prompt ("Inserisca la sua età")
 console.log(ageCostumer);
+
 //<!-- - avere una variabile con il prezzo (€0.21/km) -->
 //- SE il passeggiero ha 17 anni o meno applicare lo sconto del 20%
 if (ageCostumer < 18) {                         
-    console.log((kmToTravel*0.21)/100*80);
-
+    console.log("Lei dovrà pagare €" +  ((kmToTravel*0.21)/100*80).toFixed(2));
+    
 }
 //    - OPPURE SE il passeggiero ha più o uguale a 65 anni applicare lo sconto del 40%
 else if(ageCostumer > 64){                    
-    console.log((kmToTravel*0.21)/100*60);
+    console.log("Lei dovrà pagare €" + ((kmToTravel*0.21)/100*60).toFixed(2));
 }
 //        - OPPURE far pagare prezzo pieno
 else{                                          
-    console.log(kmToTravel*0.21);
+    console.log("Lei dovrà pagare €" + (kmToTravel*0.21).toFixed(2));
 }
+
 //- Far visualizzare l'output desiderato con massimo due cifre decimali e il simbolo €
+// document.writeln
