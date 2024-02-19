79d0078fa6e7baeb3d8775d94c3121bb1761a58a
StefDev1801 <stefano200118@gmail.com>
2024-02-19 18:32:55 +0100

    add alert

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,17 +8,19 @@ console.log(typeof ageCostumer, typeof kmToTravel);
 //<!-- - avere una variabile con il prezzo (€0.21/km) -->
 //- SE il passeggiero ha 17 anni o meno applicare lo sconto del 20%
 if (ageCostumer < 18) {                         
-    console.log("Lei dovrà pagare €" +  ((kmToTravel*0.21)/100*80).toFixed(2));
-
+    const ticketPrice = (((kmToTravel*0.21)/100*80).toFixed(2));
+    alert("Lei dovrà pagare €" + ticketPrice)
 }
 //    - OPPURE SE il passeggiero ha più o uguale a 65 anni applicare lo sconto del 40%
-else if(ageCostumer > 64){                    
-    console.log("Lei dovrà pagare €" + ((kmToTravel*0.21)/100*60).toFixed(2));
+else if(ageCostumer >= 65){                    
+    const ticketPrice = (((kmToTravel*0.21)/100*60).toFixed(2));
+    alert("Lei dovrà pagare €" + ticketPrice)
 }
 //        - OPPURE far pagare prezzo pieno
 else{                                          
-    console.log("Lei dovrà pagare €" + (kmToTravel*0.21).toFixed(2));
+    const ticketPrice = ((kmToTravel*0.21).toFixed(2));
+    alert("Lei dovrà pagare €" + ticketPrice)
 }
-
 //- Far visualizzare l'output desiderato con massimo due cifre decimali e il simbolo €
+alert("Lei dovrà pagare €" + ticketPrice)
 // document.writeln, creare una sola variabile per accorpare il prezzo?
