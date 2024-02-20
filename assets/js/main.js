fb3f8458fd4826ccef0dd7c3bcb5b175c0507137
StefDev1801 <stefano200118@gmail.com>
2024-02-20 10:16:46 +0100

    fix js from multiple alert to 1 outside if

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,19 +7,18 @@ console.log("Età: " + ageCostumer,", chilometri da percorrere: " + kmToTravel);
 console.log(typeof ageCostumer, typeof kmToTravel);
 //<!-- - avere una variabile con il prezzo (€0.21/km) -->
 //- SE il passeggiero ha 17 anni o meno applicare lo sconto del 20%
+let ticketPrice;
 if (ageCostumer < 18) {                         
-    const ticketPrice = (((kmToTravel*0.21)/100*80).toFixed(2));
-    alert("Lei dovrà pagare €" + ticketPrice);
+     ticketPrice = (((kmToTravel*0.21)/100*80).toFixed(2));
 }
 //    - OPPURE SE il passeggiero ha più o uguale a 65 anni applicare lo sconto del 40%
 else if(ageCostumer >= 65){                    
-    const ticketPrice = (((kmToTravel*0.21)/100*60).toFixed(2));
-    alert("Lei dovrà pagare €" + ticketPrice);
+     ticketPrice = (((kmToTravel*0.21)/100*60).toFixed(2));
 }
 //        - OPPURE far pagare prezzo pieno
 else{                                          
-    const ticketPrice = ((kmToTravel*0.21).toFixed(2));
-    alert("Lei dovrà pagare €" + ticketPrice);
+     ticketPrice = ((kmToTravel*0.21).toFixed(2));
 }
+alert("Lei dovrà pagare €" + ticketPrice);
 //- Far visualizzare l'output desiderato con massimo due cifre decimali e il simbolo €
 // document.writeln, creare una sola variabile per accorpare il prezzo?
--- readme.md
+++ readme.md
@@ -8,13 +8,16 @@ va applicato uno sconto del 40% per gli over 65 (fortunelli :adulto_anziano:).
 L'output del prezzo finale va messo fuori in forma umana (con massimo due cifre decimali, per indicare centesimi sul prezzo).
 Questo richiederà un minimo di ricerca - come quelle fatte in classe :occhiolino:
 
-## Steps
+## PseudoCode
 - Chiedere chilometri che il passeggiero dovrà percorrere
 - Chiedere età del passeggiero
 <!-- - avere una variabile con il prezzo (€0.21/km) -->
-- SE il passeggiero ha 17 anni o meno applicare lo sconto del 20%
-    - OPPURE SE il passeggiero ha più o uguale a 65 anni applicare lo sconto del 40%
-        - OPPURE far pagare prezzo pieno
+- SE il passeggiero ha 17 anni o meno
+    applicare lo sconto del 20%
+- OPPURE SE il passeggiero ha più o uguale a 65 anni
+    applicare lo sconto del 40%
+- OPPURE 
+    far pagare prezzo pieno
 - Far visualizzare l'output desiderato con massimo due cifre decimali e il simbolo €
 
 ### Tools
